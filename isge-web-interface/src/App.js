bebfce95cc3aace5da5fd47e34067649504c7cfb
Szymon Bujowski <s.bujowski@gmail.com>
2023-01-16 15:57:44 +0100

    resolve '-' to '_' issue in JSON representation, decide to go fix set of basic interactions

--- isge-web-interface/src/App.js
+++ isge-web-interface/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
 function App() {
+  // todo - load/start screen
+  const gamefile = require('./horror-story.json')
+
   return (
     <div className="App">
-      Foo
+      {gamefile.scenes.cell_room.name}
     </div>
   );
 }
--- isge-web-interface/src/todo-present.js
+++ isge-web-interface/src/todo-present.js
@@ -1,13 +1,19 @@
-// todo - fixed set of possible interaction commands for all objects OR list interactions?
+// Fixed set of possible interaction commands for all objects OR list interactions?
+//  - fixed set of basic interactions -> imagination, exploration BUT restrictions on what kinds of interactions are possible
+//  - list them -> more possible interactions BUT deprives the player of exploration and imagination (everything is laid out)
+//  A: fixed set of basic interactions - more interesting and valuable despite the restrictions
+// todo
+
+// todo - typewriter (windup) effect - https://github.com/sgwilym/windups
 
 // Navigation from within objects interaction (e.g. door) OR separate property of a scene
 //  - maybe combined?
-//  - A: (player - object/navigation division, game format - separate "all navigation"
+//  A: (player - object/navigation division, game format - separate "all navigation"
 // todo
 
 // How to handle the conditions?
 //  - global list of objects/entities etc.? (most likely)
 //  - access properties of scenes?
 //  - how to encode in the JSON?
-//  - A: backend "oracle" (gather all conditions strings, parse onLoad, handle calls for respective conditions [maybe as key-value assignment afterParsed pairs?])
+//  A: backend "oracle" (gather all conditions strings, parse onLoad, handle calls for respective conditions [maybe as key-value assignment afterParsed pairs?])
 // todo
